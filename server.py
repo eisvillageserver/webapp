06c0441e26b89c62028482bab942fc605748f36d
Eleanor Wong <Eleanor Wong>
2016-01-08 15:03:31 -0800

    Initial server code, Foundation Getting Started Page

---
+++ server.py
@@ -0,0 +1,11 @@
+import SimpleHTTPServer
+import SocketServer
+
+port = 1234
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+http = SocketServer.TCPServer(("", port), Handler)
+
+print "SERVING WEBAPP ON PORT", port
+
+http.serve_forever()
