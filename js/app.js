7e1a7d1c44d4c58be2afd29923a61217c1216a99
eleanorwong <eleanorwong@live.ca>
2016-03-11 16:27:48 -0800

    Up to date working web app, only document downloading is implemented so far

--- js/app.js
+++ js/app.js
@@ -1,1 +1,56 @@
-$(document).foundation();
+var app = angular.module('eisApp', ['ngRoute']);
+
+host = 'http://localhost:5000/'
+
+app.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl'
+    })
+    .when('/Document', {
+      templateUrl: 'views/docs.html',
+      controller: 'DocsCtrl'
+    })
+    .when('/Image', {
+      templateUrl: 'views/images.html',
+      controller: 'ImgCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+    // $locationProvider.html5Mode({
+    //   enabled: true,
+    //   requireBase: false
+    // });
+});
+
+app.controller('MainCtrl', function($scope, $http) {
+  $scope.loading = true;
+  $http.get(host + 'categories/').then(function(response) {
+    $scope.categories = response.data.categories;
+    $scope.loading = false;
+  })
+})
+
+app.controller('DocsCtrl', function($scope, $http) {
+  $scope.loading = true;
+  $http.get(host + 'categories/documents').then(function(response) {
+    console.log("HELLO");
+    console.log(response);
+    $scope.documents = response.data.files;
+    $scope.loading = false;
+  })
+})
+
+app.controller('FilesCtrl', function($scope, $http) {
+  $http.get(host + 'categories/').then(function(response) {
+    console.log(response);
+    $scope.categories = response.data.categories;
+  })
+
+  $http.get('files/' + '5').then(function(response) {
+    console.log(response);
+    $scope.currentItem = response.data.files[0];
+  })
+})
